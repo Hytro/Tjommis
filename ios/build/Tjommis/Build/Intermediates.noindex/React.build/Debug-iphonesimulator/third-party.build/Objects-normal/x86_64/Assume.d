48f065351a46a111af9f2e659a2c6dedaa150e25
goldoke101 <sondregullik@gmail.com>
2019-05-29 13:56:23 +0200

    ios
    shit

--- ios/build/Tjommis/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/third-party.build/Objects-normal/x86_64/Assume.d
+++
@@ -1,9 +0,0 @@
-dependencies: \
-  /Users/zachosekken/Desktop/Tjommis2/node_modules/react-native/third-party/folly-2018.10.22.00/folly/lang/Assume.cpp \
-  /Users/zachosekken/Desktop/Tjommis2/node_modules/react-native/third-party/folly-2018.10.22.00/folly/lang/Assume.h \
-  /Users/zachosekken/Desktop/Tjommis2/node_modules/react-native/third-party/folly-2018.10.22.00/folly/Portability.h \
-  /Users/zachosekken/Desktop/Tjommis2/node_modules/react-native/third-party/folly-2018.10.22.00/folly/portability/Config.h \
-  /Users/zachosekken/Desktop/Tjommis2/node_modules/react-native/third-party/folly-2018.10.22.00/folly/CPortability.h \
-  /Users/zachosekken/Desktop/Tjommis2/node_modules/react-native/third-party/glog-0.3.5/exported/glog/logging.h \
-  /Users/zachosekken/Desktop/Tjommis2/node_modules/react-native/third-party/glog-0.3.5/exported/glog/log_severity.h \
-  /Users/zachosekken/Desktop/Tjommis2/node_modules/react-native/third-party/glog-0.3.5/exported/glog/vlog_is_on.h
