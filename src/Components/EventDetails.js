832afd531a96a4d970ee0982554fc28cd33ca6bc
goldoke101 <sondregullik@gmail.com>
2019-06-04 16:13:04 +0200

    Update EventDetails.js

--- src/Components/EventDetails.js
+++ src/Components/EventDetails.js
@@ -8,25 +8,25 @@ import axios from 'axios';
 class EventDetails extends React.PureComponent {
 
 
-    async getProfile(){
-    
-          var url = 'http://tjommis.eu-central-1.elasticbeanstalk.com/api/users/30';
-          const response = await axios.get(url);
-          if(response.status === 200) {
+    async getProfile() {
+
+        var url = 'http://tjommis.eu-central-1.elasticbeanstalk.com/api/users/30';
+        const response = await axios.get(url);
+        if (response.status === 200) {
             console.warn("Dank boi works")
-          }
-      }
+        }
+    }
 
 
-    
+
     render() {
         return (
             <View style={styles.card}>
-                <Text style={styles.cardTitle}>Dank</Text>
                 <Image style={styles.cardImage} source={{ uri: 'https://www.mch-group.com/-/media/mch-group/Images/Content/News/Blog/2017/2017-04/mch-group-live-marketing-aktivierung.jpg' }} />
-                <Text style={styles.cardText}>Dank</Text>
-                <Text style={styles.cardTextBody}>Dank</Text>
-                <Text style={styles.cardTextBody}>Dank</Text>
+                <Text style={styles.cardTitle}>Dank Titel</Text>
+                <Text style={styles.cardText}>Dank lokasjon</Text>
+                <Text style={styles.cardTextBody}>Info om evenet</Text>
+                <Text style={styles.cardTextBody}>Dank beskrivelse av eventet</Text>
             </View>
         );
     }
@@ -34,21 +34,40 @@ class EventDetails extends React.PureComponent {
 
 const styles = StyleSheet.create({
     card: {
-        backgroundColor: 'lightgray',
-        marginBottom: 10,
-        marginLeft: '2%',
-        width: '96%',
+        backgroundColor: 'white',
+        marginBottom: 20,
+        marginLeft: '5%',
+        width: '90%',
+        borderWidth: 0.1,
+        borderRadius: 6,
         shadowColor: '#000',
-        shadowOpacity: 0.2,
-        shadowRadius: 1,
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
         shadowOffset: {
-            width: 3,
-            height: 3
+            width: 0,
+            height: 0
         }
     },
+    flexRow: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '98%',
+        marginLeft: '1%'
+    },
+    flexRowBottom: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '85%',
+        marginLeft: '7.5%',
+        paddingTop: 7,
+        paddingBottom: 7
+    },
     cardImage: {
-        width: '100%',
-        height: 200,
+        width: '95%',
+        marginLeft: '2.5%',
+        height: 125,
         resizeMode: 'cover'
     },
     cardText: {
@@ -62,6 +81,12 @@ const styles = StyleSheet.create({
     cardTitle: {
         padding: 4,
         fontSize: 20
+    },
+    cardDate: {
+        paddingTop: 15,
+        paddingRight: 5,
+        opacity: 0.6,
+        fontSize: 12
     }
 });
 
