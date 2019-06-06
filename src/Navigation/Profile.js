432040bce96f7ddab654babb10032813932b86f1
goldoke101 <sondregullik@gmail.com>
2019-06-06 11:30:34 +0200

    update shit

--- src/Navigation/Profile.js
+++ src/Navigation/Profile.js
@@ -9,7 +9,7 @@ class Profile extends Component {
         <Image style={styles.avatar} source={{ uri: 'https://bootdey.com/img/Content/avatar/avatar6.png' }} />
         <View style={styles.body}>
           <View style={styles.bodyContent}>
-            <Text style={styles.name}>John Doe</Text>
+            <Text style={styles.name}>John Doe, 22</Text>
             <Text style={styles.info}>UX Designer / Mobile developer</Text>
             <Text style={styles.description}>Lorem ipsum dolor sit amet, saepe sapientem eu nam. Qui ne assum electram expetendis, omittam deseruisse consequuntur ius an,</Text>
 
