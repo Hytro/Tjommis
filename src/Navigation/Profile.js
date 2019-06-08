3cef11f258424c0c98b4b0e9df8f6918901a816b
Andersmidtbo <andersmid@gmail.com>
2019-06-08 13:05:38 +0200

    load profile

--- src/Navigation/Profile.js
+++ src/Navigation/Profile.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
 import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import axios from 'axios';
 
 class Profile extends Component {
+  state = {
+    user: {
+      firstName: '-',
+      lastName: '-',
+      image_url: '-',
+    }
+  }
+
+  async componentDidMount() {
+    const token = await AsyncStorage.getItem('token');
+    const userResponse = await axios.get(`http://tjommis.eu-central-1.elasticbeanstalk.com/api/auth/me`, {token})
+    this.setState({user: userResponse.data})
+  }
   render() {
-
-
-
-
-
-
-
-
-
-
     return (
       <View style={styles.container}>
         <View style={styles.header}></View>
         <Image style={styles.avatar} source={{ uri: 'https://bootdey.com/img/Content/avatar/avatar6.png' }} />
         <View style={styles.body}>
           <View style={styles.bodyContent}>
-            <Text style={styles.name}>John Doe, 22</Text>
+            <Text style={styles.name}>{`${this.state.user.firstName} ${this.state.user.lastName}`}, 22</Text>
             <Text style={styles.info}>UX Designer / Mobile developer</Text>
             <Text style={styles.description}>Lorem ipsum dolor sit amet, saepe sapientem eu nam. Qui ne assum electram expetendis, omittam deseruisse consequuntur ius an,</Text>
 
